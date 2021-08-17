7d6ef9e99bb67d66c0959fb7354cdfe788636a16
Nahid Hasan <nahidhassan.jony@gmail.com>
2021-08-17 10:48:49 +0600

    add extra text

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * @package Fiberr
+ */
+
+get_header();
+?>
+
+<main style="margin-top: 80px;">
+<div class="theme-main-wrap">
+	<div class="theme-center-section">
+		<div class="container">
+		<div class="theme-center-wrap">
+			<div class="theme-center-left">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, explicabo illum. Hic ipsam quae, ex sequi nihil neque quod corporis molestias id, perspiciatis officia fugiat? Blanditiis adipisci, sapiente laboriosam laudantium dolorem veritatis placeat fuga accusantium eligendi repellat vel quasi nesciunt animi ullam reiciendis fugit maxime harum minus laborum doloremque voluptate doloribus minima magnam. Nisi voluptates consequuntur possimus voluptatum iure, veritatis recusandae nemo corrupti error eum rem sapiente quo sit, provident doloremque commodi fuga. Qui minima esse sapiente iste laborum obcaecati sequi, officiis repudiandae quia ipsam asperiores eos! Nihil quidem minus voluptatem in ducimus nobis quos. Soluta culpa itaque minima quas.
+			</div> alahahs sh
+			<?php get_sidebar(); ?>
+		</div>
+		</div>
+	</div>      
+</div> <!-- .theme-main-wrap -->
+</main><!-- main -->
+
+<?php
+get_sidebar();
+get_footer();
